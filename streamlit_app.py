eb8a6caf6bcdc8ea3e1860be2f09b183523fcb95
Surya-75 <124672291+Surya-75@users.noreply.github.com>
2023-02-08 12:36:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,3 @@
 Import streamlit
 streamlit.title('My parents Helathy Dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('--Thank You--')
